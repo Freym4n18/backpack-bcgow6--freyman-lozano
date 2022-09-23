ac355f7d3b91ba99b8de40cbe034c1c1e4e76f7a
Freyman Lozano <freyma.lozano@mercadolibre.com.co>
2022-09-23 11:11:36 -0500

    Primera practica go - morning

---
+++ C1 - Go - Mañana/Ejercicio2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	temperatura := 20.4
+	presion := 1035.4
+	humedad := 17.4
+	fmt.Println("Temperatura: ", temperatura)
+	fmt.Println("Presion: ", presion)
+	fmt.Println("Humedad: ", humedad)
+}
+	
