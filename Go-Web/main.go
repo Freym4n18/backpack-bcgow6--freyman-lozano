2f8d066cb71c3e8c1ce123f7bfa7d14364350b15
Freyman Lozano <freyma.lozano@mercadolibre.com.co>
2022-10-03 15:44:36 -0500

    Go web segunda practica

--- Go-Web/main.go
+++ Go-Web/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"encoding/json"
 	"os"
+	"net/http"
+	"strconv"
+	"fmt"
 )
 
 type Product struct {
@@ -33,7 +36,93 @@ func GetAll(c *gin.Context) {
 	check(err)
     err = json.Unmarshal(productByteArray, &products)
 	check(err)
-	c.JSON(200,&products)
+
+	//lets start with the filters
+	name := c.Request.URL.Query().Get("name")
+	color := c.Request.URL.Query().Get("color")
+	stock := c.Request.URL.Query().Get("stock")
+	code := c.Request.URL.Query().Get("code")
+	published := c.Request.URL.Query().Get("published")
+	afterDate := c.Request.URL.Query().Get("after_date")
+	price := c.Request.URL.Query().Get("price")
+	
+
+	fmt.Println(name)
+	productsFiltered := []Product{}
+
+	for _, product := range products {
+		addproduct := true
+		if name!= "" {
+            if product.Name!= name {
+				addproduct = false
+			}
+		}
+		if color!= "" {
+            if product.Color!= color {
+				addproduct = false
+			}
+		}
+		if stock!= "" {
+			stockInt, err := strconv.Atoi(stock)
+			check(err)
+            if product.Stock!= stockInt {
+				addproduct = false
+            }
+		}
+		if code!= "" {
+			codeInt, err := strconv.Atoi(code)
+			check(err)
+            if product.Code!= codeInt {
+				addproduct = false
+			}
+		}
+		if published!= "" {
+			publishedBool, err := strconv.ParseBool(published)
+            check(err)
+            if product.Published!= publishedBool {
+                addproduct = false
+            }
+		}
+		if afterDate!= "" {
+			createDateConv, err := time.Parse("2006-01-02", afterDate)
+            check(err)
+            if product.CreateDate.Before(createDateConv) {
+                addproduct = false
+            }
+		}
+		if price!= "" {
+			priceFloat, err := strconv.ParseFloat(price, 64)
+            check(err)
+            if product.Price!= priceFloat {
+                addproduct = false
+            }
+		}
+		if addproduct {
+            productsFiltered = append(productsFiltered, product)
+		}
+	}
+
+	c.JSON(http.StatusAccepted,&productsFiltered)
+}
+
+func GetOne(c *gin.Context) {
+	id := c.Param("id")
+    products := []Product{}
+    jsonFile, err := os.Open("./Products.json")
+    check(err)
+    productByteArray, err := io.ReadAll(jsonFile)
+    check(err)
+	err = json.Unmarshal(productByteArray, &products)
+    check(err)
+	for _, product := range products {
+        if product.Id == id {
+			c.JSON(200,&product)
+            return
+		}
+	}
+	c.JSON(http.StatusAccepted, gin.H{
+		"message": "Product ID not found",
+	})
 }
 
 func main() {
@@ -46,6 +135,8 @@ func main() {
 	})
 
 	router.GET("/products", GetAll)
+
+	router.GET("/products/:id", GetOne)
 
 	router.Run(":8080")
 }
