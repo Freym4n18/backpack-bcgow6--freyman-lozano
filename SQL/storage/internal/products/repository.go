e667413d0dae4881d7d70644a5bf06f5fb3dac5b
Freyman Lozano <freyma.lozano@mercadolibre.com.co>
2022-11-09 13:06:34 -0500

    practica 2-1 implementacion base de datos

--- SQL/storage/internal/products/repository.go
+++ SQL/storage/internal/products/repository.go
@@ -2,28 +2,34 @@ package products
 
 import (
 	"database/sql"
+	"errors"
+	"log"
 
 	"github.com/Freym4n18/backpack-bcgow6--freyman-lozano/SQL/storage/internal/domain"
 )
 
 const (
 	GET_PRODUCT_BY_NAME = "SELECT id, name, type, count, price FROM products WHERE name=?;"
+
+	GET_PRODUCT_BY_ID = "SELECT id, name, type, count, price FROM products WHERE id=?;"
+	
 	SAVE_PRODUCT =  "INSERT INTO products (name, type, count, price) VALUES (?, ?, ?, ?);"
 
-	GET_ALL_MOVIES = ""
+	GET_ALL_PRODUCTS = "SELECT id, name, type, count, price FROM products"
 
-	GET_MOVIE = "SELECT id, title, rating, awards, length, genre_id FROM movies WHERE id=?;"
+	UPDATE_PRODUCT = "UPDATE products SET name=?, type=?, count=?, price=? WHERE id=?;"
 
-	UPDATE_MOVIE = "UPDATE movies SET title=?, rating=?, awards=?, length=?, genre_id=? WHERE id=?;"
+	DELETE_PRODUCT = "DELETE FROM products WHERE id=?"
 
-	DELETE_MOVIE = ""
-
-	EXIST_MOVIE = "SELECT m.id FROM movies m WHERE m.id=?"
 )
 
 type Repository interface {
 	GetByName(name string) (domain.Product, error)
 	Store(p domain.Product) (int64, error)
+	Update(p domain.Product, id int) (error)
+	Delete(id int) (error)
+	GetAll() ([]domain.Product, error)
+	GetById(id int) (domain.Product, error)
 }
 
 type repository struct {
@@ -39,6 +45,72 @@ func (r *repository) GetByName(name string) (domain.Product, error) {
 	return product, nil
 }
 
+func (r *repository) GetById(id int) (domain.Product, error) {
+	row := r.db.QueryRow(GET_PRODUCT_BY_ID, id)
+	var product domain.Product
+	if err := row.Scan(&product.Id, &product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+		return domain.Product{}, err
+	}
+	return product, nil
+}
+
+func (r *repository) Delete(id int) (error) {
+	stmt, err := r.db.Prepare(DELETE_PRODUCT)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) GetAll() ([]domain.Product, error) {
+	var products []domain.Product
+	rows, err := r.db.Query(GET_ALL_PRODUCTS)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	
+	for rows.Next() {
+		var product domain.Product
+		if err := rows.Scan(&product.Id, &product.Name, &product.Type, &product.Count, &product.Price); err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	return products, nil
+}
+
+
+func (r *repository) Update(p domain.Product, id int) error {
+	stm, err := r.db.Prepare(UPDATE_PRODUCT)
+	if err != nil {
+		return err
+	}
+	defer stm.Close() //cerramos para no perder memoria
+
+	//ejecutamos la consulta con aquellos valores a remplazar en la sentencia
+	result, err := stm.Exec(p.Name, p.Type, p.Count, p.Price, id)
+	if err != nil {
+		return err
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if affected < 1 {
+		return errors.New("error: no affected rows")
+	}
+	return nil
+}
 func (r *repository) Store(p domain.Product) (int64, error) {
 	stm, err := r.db.Prepare(SAVE_PRODUCT) //preparamos la consulta
 	if err != nil {
--- SQL/storage/internal/products/repository__test.go
+++ SQL/storage/internal/products/repository__test.go
@@ -24,6 +24,61 @@ func TestGetByName(t *testing.T) {
 	assert.Equal(t, product, result)
 }
 
+func TestGetAll(t *testing.T) {
+	//arrange
+	_, db := db.ConnectDatabase()
+	repo := NewRepository(db)
+	result,err := repo.GetAll()
+	assert.NoError(t,err)
+	assert.Greater(t, len(result), 0)
+}
+
+func TestUpdate(t *testing.T) {
+	//arrange
+	product := domain.Product{
+		Name: "Laptop",
+		Type: "Technology",
+		Count: 3,
+		Price: 2000.0,
+	
+	}
+	_, db := db.ConnectDatabase()
+	repo := NewRepository(db)
+	result,err := repo.Store(product)
+	assert.NoError(t,err)
+	assert.Greater(t, int(result), 0)
+	product.Id = result
+	product.Name = "Update"
+	product.Count = 4
+	err = repo.Update(product, int(product.Id))
+	assert.NoError(t, err)
+	updated,err := repo.GetById(int(result))
+	assert.NoError(t, err)
+	assert.Equal(t, product, updated)
+}
+
+func TestDelete(t *testing.T) {
+	//arrange
+	product := domain.Product{
+		Name: "Laptop",
+		Type: "Technology",
+		Count: 3,
+		Price: 2000.0,
+	
+	}
+	_, db := db.ConnectDatabase()
+	repo := NewRepository(db)
+	result,err := repo.Store(product)
+	assert.NoError(t,err)
+	assert.Greater(t, int(result), 0)
+
+	err = repo.Delete(int(result))
+	assert.NoError(t,err)
+	product, err = repo.GetById(int(result))
+	assert.Error(t, err)
+	assert.Empty(t, product)
+}
+
 func TestStore(t *testing.T) {
 	//arrange
 	product := domain.Product{
